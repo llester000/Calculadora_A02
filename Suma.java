f74716919f020079b0155112271968e2eeb8835a
llester000 <guillermo.olivaresbernat@gmail.com>
2025-01-16 23:16:09 +0100

    Creacion de los 2 ultimos metodos de suma de 3 numeros

--- Suma.java
+++ Suma.java
@@ -8,4 +8,12 @@ public class Suma {
     public double sumaDosEnteros(int num1, int num2) {
         return num1 + num2;
     }
+
+    public double sumaTresReales(double num1, double num2, double num3) {
+        return num1 + num2 + num3;
+    }
+
+    public double sumaTresEnteros(int num1, int num2, int num3) {
+        return num1 + num2 + num3;
+    }
 }

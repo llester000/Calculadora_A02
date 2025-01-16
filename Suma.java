b8c84b78deb80d9ea164f5fb1f35994492b13578
llester000 <guillermo.olivaresbernat@gmail.com>
2025-01-16 22:45:14 +0100

    Creacion de los 2 primeros metodos de suma de 2 numeros

--- Suma.java
+++ Suma.java
@@ -1,2 +1,11 @@
 public class Suma {
+    private double valorFinal;
+
+    public double sumaDosReales(double num1, double num2) {
+        return num1 + num2;
+    }
+
+    public double sumaDosEnteros(int num1, int num2) {
+        return num1 + num2;
+    }
 }

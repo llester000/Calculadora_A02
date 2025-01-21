db3450be500dbec411c34d71c93024d17192a959
llester000 <guillermo.olivaresbernat@gmail.com>
2025-01-21 19:55:27 +0100

    Commit para guardar los comentarios de la clase main

--- Main.java
+++ Main.java
@@ -1,11 +1,24 @@
+/**
+ * Clase Main.
+ * Esta clase prueba los metodos relacionados con las opciones de suma de la calculadora
+ * <p>
+ * Autor: Guillermo Olivares Bernat y su compañero
+ * GitHub: llester000 y CompaneroGuillermo
+ */
 public class Main {
     public static void main(String[] args) {
 
+        /**
+         * Creacion de los objetos para poder probar los metodos
+         */
         Suma suma = new Suma();
         Resta resta = new Resta();
         Producto producto = new Producto();
         Cociente cociente = new Cociente();
 
+        /**
+         *Salida de los metodos de la clase suma
+         */
         System.out.println("Suma de dos reales: " + suma.sumaDosReales(4.6, 2.5));
         System.out.println("Suma de dos enteros: " + suma.sumaDosEnteros(4, 2));
         System.out.println("Suma de tres reales: " + suma.sumaTresReales(4.6, 2.5, 1.5));
@@ -13,8 +26,9 @@ public class Main {
         System.out.println("Suma acumulativa: " + suma.sumaAcum(4));
         System.out.println("---------------------------------------");
 
-
-
+        /**
+         *Salida de los metodos de la clase resta
+         */
 
         System.out.println("Resta de dos reeales: " + resta.restaDosReales(4.6, 2.5));
         System.out.println("Resta de dos enteros: " + resta.restaDosEnteros(4, 2));
@@ -22,10 +36,19 @@ public class Main {
         System.out.println("Resta de numero acumulado:" + resta.restaAcum(2.6));
         System.out.println("Resta de numero acumulado:" + resta.restaAcum(2.6));
         System.out.println("---------------------------------------");
+
+        /**
+         *Salida de los metodos de la clase producto
+         */
+
         System.out.println("Producto de dos reales: " + producto.productoDosReales(1.3,4.5));
         System.out.println("Producto de dos enteros: " + producto.productoDosEnteros(2,86));
         System.out.println("Producto de tres reales: " + producto.productoTresReales(1.3,4.5,1.5));
         System.out.println("Potencia: " + producto.potencia(2,3));
+
+        /**
+         *Salida de los metodos de la clase cociente
+         */
 
         System.out.println("---------------------------------------");
         System.out.println("Cociente de dos reales: " + cociente.cocienteDosReales(4.0, 2.0));

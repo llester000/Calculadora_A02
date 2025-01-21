4b3f2931daaa709f251e7ea0210c138c4c88d5c4
llester000 <guillermo.olivaresbernat@gmail.com>
2025-01-21 18:51:23 +0100

    Commit para guardar la creacion del fichero Main y de las primeras pruebas de Guillermo Olivares Bernat

---
+++ Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Resta resta = new Resta();
+
+        System.out.println("Resta de dos reeales: " + resta.restaDosReales(4.6, 2.5));
+    }
+}

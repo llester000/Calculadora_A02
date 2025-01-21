da844f8a07ee0df1045fdba2bf223badf42e8153
llester000 <guillermo.olivaresbernat@gmail.com>
2025-01-21 17:47:43 +0100

    Commit para guardar todos los modelos de la clase Producto.java

--- Producto.java
+++ Producto.java
@@ -1,3 +1,20 @@
 public class Producto {
 
+    public double productoDosReales(double num1, double num2) {
+        return num1 * num2;
+    }
+
+    public int productoDosEnteros(int num1, int num2){
+        return num1 * num2;
+    }
+    public double productoTresReales(double num1, double num2, double num3) {
+        return num1 * num2 * num3;
+    }
+    public double productoAcum(double base, double exponente) {
+        double resultado = 1;
+        for (int i = 0; i < exponente; i++) {
+            resultado *= base;
+        }
+        return resultado;
+    }
 }
